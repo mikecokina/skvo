5a0a50f318baa530d0ada2b298b4aa31ac5df480
s-t-e-a-l-t-h <mikecokina@gmail.com>
2018-10-08 22:31:43 +0200

    observation service - basics

---
+++ skvo/observation_metaservice/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Target(models.Model):
+    pass
+
+
+class Bandpass(models.Model):
+    pass
+
+
+class Observation(models.Model):
+    pass
+
+
+class Facility(models.Model):
+    pass
+
+
+class Instrument(models.Model):
+    pass
+
+
+class DataId(models.Model):
+    pass
+
+
+class Organisation(models.Model):
+    pass
+
+
+class ObservationConfiguration(models.Model):
+    pass
